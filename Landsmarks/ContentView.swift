b8cb85e539c69560df2d830f459267bc4e15a4f6
IgorDenisiuk <73896271+IgorDenisiuk@users.noreply.github.com>
2021-04-21 23:50:10 +0300

    Initial Commit

---
+++ Landsmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landsmarks
+//
+//  Created by Igor Denisiuk on 21.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landsmarks/LandsmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandsmarksApp.swift
+//  Landsmarks
+//
+//  Created by Igor Denisiuk on 21.04.21.
+//
+
+import SwiftUI
+
+@main
+struct LandsmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
