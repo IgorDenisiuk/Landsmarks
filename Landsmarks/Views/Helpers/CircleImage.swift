14e933c2fdfe2b2810d8bccfdf85f5aad0a6150a
IgorDenisiuk <73896271+IgorDenisiuk@users.noreply.github.com>
2021-04-27 14:00:59 +0300

    Added seven landmarks

---
+++ Landsmarks/Views/Helpers/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landsmarks
+//
+//  Created by Igor Denisiuk on 22.04.21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("pripiatskij"))
+    }
+}
